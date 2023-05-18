0fe292a7db9e9f0a0fd59eaeb5bad1df8909baf4
Park yeo jin <contrail020812@naver.com>
2023-05-19 05:04:42 +0900

    initial commit

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ message = "hello"
 num1 = 7
 num2 = 3.5
 print(message, num1, num2)
+
 # 입출력
 user_message = input('메세지를 입력하세요: ')
 print(user_message)
+
 # 조건문
 user = input('가위/바위/보 중 하나를 내세요.: ')
 comp = "바위" #그냥 간단히 바위만 내게 설정
@@ -15,6 +17,7 @@ elif user == "바위":
     print("비겼습니다!")
 else:
     print("당신이 이겼습니다!")
+
 # 반복문
 for i in range(0, 5):
     print("Hello world!")
